559431c22d0f60a22a5809c16b759bc8690f2bfe
Veronica Ray <vray@linkedin.com>
2014-10-25 21:53:25 -0700

    longterm -> long_term in admin

--- app/admin/home.rb
+++ app/admin/home.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Home do
 
-permit_params :name, :street, :city, :state, :longterm, :family, :directions,
+permit_params :name, :street, :city, :state, :long_term, :family, :directions,
               :comment, :latitude, :longitude, :country
 end
