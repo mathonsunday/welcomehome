6eafb8d516fc08aefb23fd5aec006933f7b095f7
Veronica Ray <vray@linkedin.com>
2014-10-25 21:25:21 -0700

    Restroom -> Home in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,15 @@
 
 require 'csv'
 
-Restroom.transaction do
+Home.transaction do
   CSV.foreach('db/export.csv') do |row|
-    Restroom.create(
+    Home.create(
       :name => row[1],
       :street => row[3],
       :city => row[4],
       :state => row[5],
-      :accessible => row[10],
-      :unisex => row[2],
+      :long_term => row[10],
+      :family => row[2],
       :directions => row[11],
       :comment => row[12],
       :latitude => row[8],
