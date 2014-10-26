2a27eef61f5fcb14ee44964fafcb4143731ae0e4
Veronica Ray <vray@linkedin.com>
2014-10-25 17:45:48 -0700

    Restroom -> Home in API

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -1,7 +1,7 @@
 module API
   module V1
     class Base < Grape::API
-      mount API::V1::Restrooms
+      mount API::V1::Homes
 
       add_swagger_documentation base_path: '/api', api_version: 'v1', hide_documentation_path: true
     end
--- app/controllers/api/v1/homes.rb
+++ app/controllers/api/v1/homes.rb
@@ -1,34 +1,34 @@
 module API
   module V1
-    class Restrooms < Grape::API
+    class Homes < Grape::API
       include Grape::Kaminari
       paginate :per_page => 10
 
       version 'v1'
       format :json
 
-      resource :restrooms do
-        desc "Get all restroom records ordered by date descending."
+      resource :homes do
+        desc "Get all home records ordered by date descending."
         params do
-          optional :ada, type: Boolean, desc: "Only return restrooms that are ADA accessible."
-          optional :unisex, type: Boolean, desc: "Only return restrooms that are unisex."
+          optional :ada, type: Boolean, desc: "Only return homes that are ADA accessible."
+          optional :unisex, type: Boolean, desc: "Only return homes that are unisex."
         end
         get do
-          r = Restroom
+          r = Home
           r = r.accessible if params[:ada].present?
           r = r.unisex if params[:unisex].present?
 
           paginate(r.order(created_at: :desc))
         end
 
-        desc "Perform full-text search of restroom records."
+        desc "Perform full-text search of home records."
         params do
-          optional :ada, type: Boolean, desc: "Only return restrooms that are ADA accessible."
-          optional :unisex, type: Boolean, desc: "Only return restrooms that are unisex."
+          optional :ada, type: Boolean, desc: "Only return homes that are ADA accessible."
+          optional :unisex, type: Boolean, desc: "Only return homes that are unisex."
           requires :query, type: String, desc: "Your search query."
         end
         get :search do
-          r = Restroom
+          r = Home
           r = r.accessible if params[:ada].present?
           r = r.unisex if params[:unisex].present?
 
@@ -37,13 +37,13 @@ module API
 
         desc "Search by location."
         params do
-          optional :ada, type: Boolean, desc: "Only return restrooms that are ADA accessible."
-          optional :unisex, type: Boolean, desc: "Only return restrooms that are unisex."
+          optional :ada, type: Boolean, desc: "Only return homes that are ADA accessible."
+          optional :unisex, type: Boolean, desc: "Only return homes that are unisex."
           requires :lat, type: Float, desc: "latitude"
           requires :lng, type: Float, desc: "longitude"
         end
         get :by_location do
-          r = Restroom
+          r = Home
           r = r.accessible if params[:ada].present?
           r = r.unisex if params[:unisex]
           paginate(r.near([params[:lat], params[:lng]], 20, :order => 'distance'))
