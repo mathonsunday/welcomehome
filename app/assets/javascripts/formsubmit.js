989a731a4e88996ea26791f1521853bc0de4ec87
Veronica Ray <vray@linkedin.com>
2014-10-25 17:20:51 -0700

    change restroom to home in formsubmit.js

--- app/assets/javascripts/formsubmit.js
+++ app/assets/javascripts/formsubmit.js
@@ -9,14 +9,14 @@ $(function () {
         if(results && results.length > 0){
           $.ajax({
             type: 'GET',
-            url: '/restrooms/new',
-            data: {guess: true, restroom: {latitude: pos.coords.latitude, longitude: pos.coords.longitude}},
+            url: '/homes/new',
+            data: {guess: true, home: {latitude: pos.coords.latitude, longitude: pos.coords.longitude}},
             success: function(data, textStatus) {
               $(".form-container").html(data);
 
               $.ajax({
                 type: 'GET',
-                url: '/restrooms',
+                url: '/homes',
                 data: {search: 'true', nearby: true, lat: pos.coords.latitude, long: pos.coords.longitude},
                 success: function(data, textStatus) {
                   $('#nearby').html(data);
